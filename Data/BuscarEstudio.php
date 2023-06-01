9800d1e04c0a02c0bc5ce04b93c33b096ab47392
Ruben Flores <78937675+Ruben539@users.noreply.github.com>
2023-06-01 12:25:39 -0400

    Prueba de los estudios.

--- Data/BuscarEstudio.php
+++ Data/BuscarEstudio.php
@@ -1,73 +1,33 @@
 <?php
-//echo 'Hola desde el buscador';
-//print_r($_POST);
-session_start();
-require_once("../Models/conexion.php");
 
+require_once('../Models/conexion.php');
+#$medico=$_POST['medico'];
 
-$fecha_desde = '';
-$fecha_hasta  = '';
+$medico = trim($_POST['medico']);
+$fecha= date("m-Y")."-2017";
+echo "<h3>Resumen de Estudios</h3>";
 
-
-$hoy = date("m-Y");
-
-$anio = date("Y");
-
-
-if (empty($_POST['fecha_desde']) && empty($_POST['fecha_hasta']) ) {
-
- //echo  $hoy.''.$estudio;
- //exit();
-
-  $sql = mysqli_query($conection,"SELECT Estudio,count(*) as micontador FROM historial WHERE Fecha LIKE '%$hoy%' group BY Estudio order by micontador");
-  //$rtotal=0;
-
-}else if(!empty($_POST['fecha_desde']) && !empty($_POST['fecha_hasta']) ){ 
-    $fecha_desde = date_create($_POST['fecha_desde']);
-    $fecha_hasta = date_create($_POST['fecha_hasta']);
-
-    $desde = date_format($fecha_desde, 'd-m-Y');
-    $hasta  = date_format($fecha_hasta, 'd-m-Y');
-       
-   $sql = mysqli_query($conection,"SELECT Estudio,count(*) as micontador FROM historial 
-   WHERE  Fecha BETWEEN '$desde' AND '$hasta'  GROUP BY Estudio order by micontador");
+echo "<table border='1'>
+<tr>
+<th>Estudio</th>
+<th>Cantidad</th>
+</tr>";
+$result3 = mysqli_query($conection,"select Estudio,count(*) as micontador from historial where Fecha like '%$fecha%' group BY Estudio order by micontador");
+$rtotal=0;
+while($row = mysqli_fetch_array($result3))
+{
+$multiplicacion=$row['Monto']*$row['micontador'];
+        echo "<tr>";
+        echo "<td><font size='2'>" . $row['Estudio'] . "</td>";
+        echo "<td><font size='2'>" . $row['micontador'] . "</td>";
+       # echo "<td><font size='2'>" . $row['Monto'] . "</td>";
+       # echo "<td><font size='2'>" . number_format($row['Monto']*$row['micontador'], 3, '.', '.') . "</td>";
+        echo "</tr>";
+        $rtotal=$rtotal+$multiplicacion ;
 }
+$rtotal= number_format($rtotal, 3, '.', '.');
 
-$resultado = mysqli_num_rows($sql);
+echo "</table>";
 
-echo ' 
-
-<thead>
-      <tr class="text-center">      
-        <th>Estudio</th>
-        <th>Cantidad</th>
-                                   
-      </tr>
-    </thead>
-    <tbody>';
-    $total = 0;
-
-  while ($data = mysqli_fetch_array($sql)){
-        $multiplicacion=$data['Monto']*$data['micontador'];;
-    echo '<tr>
-             <td>'. $data['Estudio']. '</td>
-             <td class="text-center">'. $data['micontador']. '</td>
-             
-
-        </tr>';
-  }
-  echo
-  $total=$total+$multiplicacion;
-  $total= number_format($total, 3, '.', '.');
-  '</tbody>
-  <tfoot>
-    <tr>
-      <td><b>Cantidad Total : </b></td>
-        
-      <td class="text-center alert alert-success">'.$total.'</td>
-      
-      
-    </tr>
-  </tfoot>
-   </table>';
+mysqli_close($con);
 ?>
