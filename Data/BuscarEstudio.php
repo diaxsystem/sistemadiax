d4bea955f6184bdb555c758416c4bc5ddad616ca
Ruben Flores <78937675+Ruben539@users.noreply.github.com>
2023-06-02 12:16:17 -0400

    Reporte de la cantidad de Estudios. Realizados en el mes por la clinica.

--- Data/BuscarEstudio.php
+++ Data/BuscarEstudio.php
@@ -1,33 +1,84 @@
+
 <?php
+require_once("../Models/conexion.php");
+$fecha_desde = '';
+$fecha_hasta  = '';
+$fecha  = '';
+$hoy = date('m-Y');
+$buscar = '';
+$where = '';
 
-require_once('../Models/conexion.php');
-#$medico=$_POST['medico'];
+if (empty($_POST['fecha_desde']) && empty($_POST['fecha_hasta'])) {
 
-$medico = trim($_POST['medico']);
-$fecha= date("m-Y")."-2017";
-echo "<h3>Resumen de Estudios</h3>";
+  $sql = mysqli_query($conection, "SELECT Estudio, COUNT(*) as cantidad FROM historial 
+   WHERE Fecha LIKE '%" . $hoy . "%'  GROUP BY Estudio ORDER BY cantidad ");
 
-echo "<table border='1'>
-<tr>
-<th>Estudio</th>
-<th>Cantidad</th>
-</tr>";
-$result3 = mysqli_query($conection,"select Estudio,count(*) as micontador from historial where Fecha like '%$fecha%' group BY Estudio order by micontador");
-$rtotal=0;
-while($row = mysqli_fetch_array($result3))
-{
-$multiplicacion=$row['Monto']*$row['micontador'];
-        echo "<tr>";
-        echo "<td><font size='2'>" . $row['Estudio'] . "</td>";
-        echo "<td><font size='2'>" . $row['micontador'] . "</td>";
-       # echo "<td><font size='2'>" . $row['Monto'] . "</td>";
-       # echo "<td><font size='2'>" . number_format($row['Monto']*$row['micontador'], 3, '.', '.') . "</td>";
-        echo "</tr>";
-        $rtotal=$rtotal+$multiplicacion ;
+
+} else if (!empty($_REQUEST['fecha_desde']) && !empty($_REQUEST['fecha_hasta'])) {
+
+  $fecha_desde = date_create($_REQUEST['fecha_desde']);
+  $desde = date_format($fecha_desde, 'd-m-Y');
+
+
+  $fecha_hasta = date_create($_REQUEST['fecha_hasta']);
+  $hasta = date_format($fecha_hasta, 'd-m-Y');
+
+  $fecha = date_format($fecha_hasta, 'm-Y');
+
+  if ($desde == $hasta) {
+
+    $where = "Fecha LIKE '%$desde%'";
+
+  } else {
+
+    $f_de = $desde . '-00:00:00';
+    $f_a  = $hasta . '-23:00:00';
+    $where = "Fecha BETWEEN '$f_de' AND '$f_a' AND Fecha LIKE '%".$fecha."%' ";
+    $buscar = "fecha_desde=$desde&fecha_hasta=$hasta ";
+
+
+  }
 }
-$rtotal= number_format($rtotal, 3, '.', '.');
 
-echo "</table>";
 
-mysqli_close($con);
+$sql = mysqli_query($conection, "SELECT Estudio, COUNT(*) as cantidad FROM historial 
+   WHERE $where  GROUP BY Estudio ORDER BY cantidad ");
+
+
+$resultado = mysqli_num_rows($sql);
+
+
+echo ' 
+
+<thead>
+      <tr class="text-center">      
+        <th>Estudio</th>
+        <th>Cantidad</th>
+                                   
+      </tr>
+    </thead>
+    <tbody>';
+$total = 0;
+
+while ($data = mysqli_fetch_array($sql)) {
+  $total += $data['cantidad'];
+  echo '<tr>
+             <td>' . $data['Estudio'] . '</td>
+             <td class="text-center">' . $data['cantidad'] . '</td>
+             
+
+        </tr>';
+}
+echo
+'</tbody>
+  <tfoot>
+    <tr>
+      <td><b>Cantidad Total : </b></td>
+    
+      <td class="text-center alert alert-success">' . $total . '</td>
+      
+      
+    </tr>
+  </tfoot>
+   </table>';
 ?>
